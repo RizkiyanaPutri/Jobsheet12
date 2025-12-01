ad94edc67a2f39718523970a8abd880e96ac1b02
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-12-01 20:57:34 +0700

    Percobaan 2 Modifikasi Pertanyaan no.6

--- Pertemuan14/Kafe22.java
+++ Pertemuan14/Kafe22.java
@@ -1,12 +1,19 @@
 package Pertemuan14;
 
 public class Kafe22 {
-    public static void menu(String namaPelanggan, boolean isMember) {
+    public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
+
+        if (kodePromo.equals("DISKON50")) { 
+            System.out.println("Kode promo valid: Diskon 50%!"); } 
+        else if (kodePromo.equals("DISKON30")) { 
+            System.out.println("Kode promo valid: Diskon 30%!"); } 
+        else { 
+            System.out.println("Kode promo invalid."); }
 
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
@@ -18,7 +25,8 @@ public class Kafe22 {
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan");
     }
+
     public static void main(String[] args) {
-        menu("Andi", true);
+        menu("Andi", true, "DISKON50");
     }
 }
