c52e8ee38aa0f7d5afc2ce233c79912a0a6ef87f
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-12-03 22:44:55 +0700

    Percobaan 5 menggunakan fungsi

--- Pertemuan14/HitungBalok22.java
+++ Pertemuan14/HitungBalok22.java
@@ -2,22 +2,32 @@ package Pertemuan14;
 import java.util.Scanner;
 
 public class HitungBalok22 {
+
+    static int hitungLuas(int pjg, int lb){
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    static int hitungVolume(int tinggi, int a, int b){
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int p, l, t, L, vol;
 
-        System.out.print("Masukkan panjang ");
+        System.out.print("Masukkan panjang: ");
         p = input.nextInt();
 
-        System.out.print("Masukkan lebar ");
+        System.out.print("Masukkan lebar: ");
         l = input.nextInt();
 
-        System.out.print("Masukkan tinggi ");
+        System.out.print("Masukkan tinggi: ");
         t = input.nextInt();
 
-        L = p * l;
+        L = hitungLuas(p, l);
         System.out.println("Luas persegi panjang adalah " + L);
-        vol = p * l * t;
+        vol = hitungVolume(t, p, l);
 
         System.out.println("Volume balok adalah " + vol);
 
